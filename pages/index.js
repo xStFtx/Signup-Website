9e6b078008fac2a203ea194b90e3f4c7349b175e
xStFtx <you@example.com>
2023-07-11 09:04:16 +0200

    1.0

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,18 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Signup() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Signup | Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Sign Up for my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Sign up for my app by filling out the required information below.
         </p>
       </main>
 
